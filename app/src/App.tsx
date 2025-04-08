82903af09f2d7be2dc162c8754cef053ddf5fe61
Dustin Wleczyk <dustinwleczyk@hotmail.com>
2025-04-07 23:38:39 -0400

    First Commit

---
+++ app/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl text-center font-bold underline">Hello World!</h1>
+  );
+}
+
+export default App;
---
+++ src/test/java/com/example/simpletasktracker/SimpletasktrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpletasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpletasktrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
